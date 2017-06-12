2d8feb432599bc1006aa85be24cffe97c2fa203a
Patrick Sheridan <sheridp2@gmail.com>
2017-06-12 13:48:51 -0700

    O lookups added

--- lab-patrick/lib/hash-table.js
+++ lab-patrick/lib/hash-table.js
@@ -3,18 +3,17 @@ const Node = require('./node')
 
 'use strict';
 
-const HashTable =module.exports = function(size = 12){
+const HashTable = module.exports = function(size = 12){
   this.size = size
   this.buckets = [...Array(this.size)]
 }
-
+// O(n)
 HashTable.prototype.hashKey = function(key){
   let hash = key.split('').reduce((acc, curr)=> acc + curr.charCodeAt(0), 0) % this.size
   return hash
 }
-
+// O(log n)
 HashTable.prototype.set = function(key, value){
-  let temp = this.buckets[this.hashKey(key)]
   if(this.buckets[this.hashKey(key)]){
     let temp =this.buckets[this.hashKey(key)]
     temp.append(new Node(value))
@@ -23,21 +22,24 @@ HashTable.prototype.set = function(key, value){
   this.buckets[this.hashKey(key)] = new DLL(new Node(value))
 
 }
-
+// O(log n)
 HashTable.prototype.get = function(key){
+  console.log(this.buckets[this.hashKey(key)]);
   return this.buckets[this.hashKey(key)]
 }
-
+// O(log n)
 HashTable.prototype.remove = function(key){
   let address = this.hashKey(key)
+  console.log('------DELETED----', this.buckets[this.hashKey(key)]);
   this.buckets[address] ? delete this.buckets[address]: new Error('key invalid')
 }
 
 let newHashTable = new HashTable();
 newHashTable.set('a', 'a')
 newHashTable.set('a', 'b')
-newHashTable.set('a', 'c')
-// newHashTable.set('c')
-// newHashTable.set('r')
-console.log(newHashTable.buckets[1]);
-console.log(newHashTable.buckets);
+newHashTable.set('b', 'b')
+newHashTable.set('b', 'c')
+newHashTable.get('a')
+newHashTable.remove('b')
+// console.log(newHashTable.buckets[1]);
+// console.log(newHashTable.buckets);
